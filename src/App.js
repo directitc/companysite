2c08ce8975d8fca4596b6106a1a270b73fe754e6
Colton McEwen <colton.mcewen@gmail.com>
2021-09-27 06:58:24 -0700

    Fixed some router and css bugs.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Services from './pages/Services/Services';
@@ -11,10 +11,11 @@ function App() {
         <div className="app">
             <Router>
                 <Switch>
-                    <Route path="/about" exact component={About} />
-                    <Route path="/services" exact component={Services} />
-                    <Route path="/contact" exact component={Contact} />
+                    <Route path="/about" component={About} />
+                    <Route path="/services" component={Services} />
+                    <Route path="/contact" component={Contact} />
                     <Route path="/" exact component={Home} />
+                    <Redirect to="/" />
                 </Switch>
             </Router>
         </div>
